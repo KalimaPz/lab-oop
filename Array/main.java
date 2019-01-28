1310b7b63ebba49eff9c090c5bf6d77c0596d057
kalima007c <kalima007c@gmail.com>
2019-01-28 17:03:18 +0700

    Added Array Study

---
+++ Array/main.java
@@ -0,0 +1,14 @@
+class main{
+    public static void main(String args[]){
+        int myArray[];
+        myArray = new int[4];
+        myArray[0] = 1;
+        myArray[1] = 2;
+        myArray[2] = 3;
+        myArray[3] = 4;
+        for(int i = 0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
+       
+    }
+} 
