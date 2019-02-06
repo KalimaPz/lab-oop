32c74a412a33fe314ff96ddd7a8a60bac5ac77c2
KalimaPz <kalima007c@gmail.com>
2019-02-06 17:26:42 +0000

    Lab Sheet

--- Lab3 Array/main.java
+++ Lab3 Array/main.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 public class main {
     public static void main(String args[]){
-        Student [] StudentList = new Student[3] ;
-            StudentList[0] = new Student("001","Donnukrit");
+        Student [] StudentList = new Student[3] ; // Intialize Object
+            StudentList[0] = new Student("001","Donnukrit"); 
             StudentList[1] = new Student("002","Teerapong");
             StudentList[2] = new Student("003","Kantapong");
                 StudentList[0].setGPA( 4.00);
