70fe88c02d98bba1e7a6b6dcfcb465d713a94910
Donnukrit Satirakul <kalima007c@gmail.com>
2019-02-14 14:49:58 +0700

    Dice Box Addd

---
+++ DiceBox/DiceBox.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class DiceBox {
+    private ArrayList<Dice>  dices;
+    
+    public void addDice(Dice d){
+        this.dices.add(d);
+    }
+    DiceBox(){
+        this.dices = new ArrayList<Dice>();
+    }
+    public int TotalFace(){
+      Dice d;
+      int total = 0 ;
+      for(int i = 0;i < dices.size(); i++){
+          d = dices.get(i);
+          total = d.getFace();
+    }
+        return total;
+}
+    public void shake(){
+        Dice d;
+        for(int i = 0; i < dices.size(); i++){
+            d = dices.get(i);
+            d.roll();
+        }
+    }
+    public void printDice(){
+        Dice d;
+        for(int i = 0; i < dices.size(); i++){
+            d = dices.get(i);
+            System.out.println("Dice "+(i+1)+" = "+d.getFace());
+        }
+    }
+}
